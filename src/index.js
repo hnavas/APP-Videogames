f998a610bc884f3fa4c00e0601105900ff99d51c
hnavas <hendri.navas@gmail.com>
2023-02-04 04:29:32 -0300

    deploy-3

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://localhost:3001';
+// axios.defaults.baseURL = 'http://localhost:3001'; //local
 axios.defaults.baseURL = 'https://appvideogamesback-production.up.railway.app/';
 
 ReactDOM.render(
